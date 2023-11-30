3bb49e878be0b35a282de68c1a2af3ca48396782
sakorh <korhsall@cubbli22-gold.local>
2023-11-30 17:34:56 +0200

    only unique usernames allowed + length requirement for passwords

--- schema.sql
+++ schema.sql
@@ -1,3 +1,10 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT,
+    role INTEGER
+);
+
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     name TEXT,
@@ -7,12 +14,6 @@ CREATE TABLE books (
     user_id INTEGER REFERENCES users
 ); 
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username TEXT,
-    password TEXT,
-    role INTEGER
-);
 
 INSERT INTO books (name, author, year, available) VALUES ('Elolliset', 'Iida Turpeinen', 2023, 0);
 INSERT INTO books (name, author, year, available) VALUES ('Talo taivaansinisellä merellä', 'TJ Klune', 2021, 0);
