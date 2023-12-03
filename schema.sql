e0868c814e94adedc65cc5dd5f4d3a15e45c7196
sakorh <korhsall@cubbli22-gold.local>
2023-12-03 23:47:49 +0200

    admin login info for testing

--- schema.sql
+++ schema.sql
@@ -20,8 +20,9 @@ CREATE TABLE reviews (
     comment TEXT,
     book_id INTEGER REFERENCES books,
     user_id INTEGER REFERENCES users
-)
+);
 
 INSERT INTO books (name, author, year, available) VALUES ('Elolliset', 'Iida Turpeinen', 2023, 0);
 INSERT INTO books (name, author, year, available) VALUES ('Talo taivaansinisellä merellä', 'TJ Klune', 2021, 0);
 INSERT INTO books (name, author, year, available) VALUES ('Mitä tekoäly on?', 'Hannu Toivonen', '2023', 0);
+INSERT INTO users (username, password, role) VALUES ('admin', 'scrypt:32768:8:1$lVh3Iz3BMFPz60xv$60593b30b2c52e397a4ab8bfb2b33ec6a1ec3b8308e95f25e79df38373751bd8f25da7ffd20c65a7296ebdffecb269e426723beb5fe96ca7c3e0039e22f2e8b0', 2);
